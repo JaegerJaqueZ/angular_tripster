d3e102f8a0c9da9b2b052f7ec3d19ec1642a6650
JaegerJaqueZ <ny_t124@hotmail.com>
2014-04-09 21:50:58 +0700

    [Add] add date_picker.js

---
+++ public/js/angular/controllers/create_trip/date_picker.js
@@ -0,0 +1,49 @@
+
+var datePickerBeginCtrl = function ($scope, createTripFactory) {
+	
+	var dateBeginTemp = createTripFactory.getChosenTrip().date_begin || new Date().getTime();
+	$scope.dateBegin  = new Date(dateBeginTemp);   
+	createTripFactory.setDateBegin(dateBeginTemp); 
+
+	$scope.format = 'dd-MMMM-yyyy';
+
+	$scope.open = function($event) {
+		$event.preventDefault();
+		$event.stopPropagation();
+		$scope.opened = true;
+	};
+
+	$scope.dateOptions = {
+		'year-format': "'yy'",
+		'starting-day': 1
+	};
+
+	$scope.changed = function(){
+		createTripFactory.setDateBegin($scope.dateBegin.getTime());			
+	}
+
+};
+
+var datePickerEndCtrl = function ($scope, createTripFactory) {
+		
+	var dateEndTemp = createTripFactory.getChosenTrip().date_end || new Date().getTime();
+	$scope.dateEnd  = new Date(dateEndTemp);   
+	createTripFactory.setDateEnd(dateEndTemp); 
+
+	$scope.format = 'dd-MMMM-yyyy';
+
+	$scope.open = function($event) {
+		$event.preventDefault();
+		$event.stopPropagation();
+		$scope.opened = true;
+	};
+
+	$scope.dateOptions = {
+		'year-format': "'yy'",
+		'starting-day': 1
+	};
+
+	$scope.changed = function(){
+		createTripFactory.setDateEnd($scope.dateEnd.getTime());			
+	}
+};
