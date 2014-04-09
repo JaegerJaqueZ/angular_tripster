c3e567c06b00c5a10a520c9f1b473996a06e1526
JaegerJaqueZ <ny_t124@hotmail.com>
2014-04-09 23:33:15 +0700

    [Add] add modal_add_trip.js that contain modalAddTripCtrl

---
+++ public/js/angular/controllers/create_trip/modal_add_trip.js
@@ -0,0 +1,130 @@
+var modalAddTripControllers = angular.module('modalAddTripControllers', []);
+
+modalAddTripControllers.controller('modalAddTripCtrl', function ($scope, $http, createTripFactory, $modal) {
+ 
+	$scope.places      = createTripFactory.getChosenTrip().places || new Array();
+	$scope.title       = createTripFactory.getChosenTrip().title || '';
+	$scope.description = createTripFactory.getChosenTrip().description || '';
+
+	$scope.$watchCollection(
+		
+		function() {
+
+			var temp = createTripFactory.getChosenTrip().places;
+			if(typeof(temp) === 'undefined') {
+				return new Array();
+			}
+
+			return temp;
+		},
+		function(newValue, oldValue) {
+			if(newValue!==oldValue) {
+				$scope.places = newValue;
+			}
+		}
+	);
+
+	$scope.open = function (place) {
+		
+		if(typeof(place) === 'undefined') {
+			//add new place
+			createTripFactory.setIsEditingPlace(false);
+			createTripFactory.setChosenPlace({});
+
+			var modalInstance = $modal.open({
+				templateUrl: 'partials/modal_add_trip_add_place.html',
+				controller: addTripAddPlaceModalInstanceCtrl,
+				backdrop: false
+			});
+		}
+		else{
+			//edit place
+			createTripFactory.setIsEditingPlace(true);
+			createTripFactory.setChosenPlace(place);
+
+			var modalInstance = $modal.open({
+				templateUrl: 'partials/modal_add_trip_edit_place.html',
+				controller: addTripEditPlaceModalInstanceCtrl,
+				backdrop: false
+			});
+		}		
+	};
+
+	$scope.save = function (){
+
+		$scope.isDisabled = true;
+
+	 	var myjson = {
+	 		"title": $scope.title,
+	 		"description": $scope.description,
+	 		"date_begin": createTripFactory.getDateBegin(),
+	 		"date_end": createTripFactory.getDateEnd(),
+	 		"user_id": createTripFactory.getUserId(),
+	 		"status": createTripFactory.PRIVATE_TRIP
+	 	};
+
+		if(createTripFactory.getIsEditingTrip()) {
+			$http({
+				method: 'PUT',
+				url: createTripFactory.getOriginPath() + "trip/update?trip_id=" + createTripFactory.getChosenTrip()._id,
+				data: myjson,
+				headers: {'Content-Type': 'application/x-www-form-urlencoded',
+				'Content-Type': 'application/json'}
+			}).
+			success(function(data, status, headers, config) {
+				createTripFactory.updateTrips();
+				createTripFactory.setIsEditingTrip(false);
+				createTripFactory.setChosenTrip({});
+				$scope.cancel();
+			}).
+			error(function(data, status, headers, config) {
+				alert("Save Failed, Please Try Again.");
+				$scope.isDisabled = false;
+			});  
+		}
+		else{
+
+			$http({
+				method: 'POST',
+				url: createTripFactory.getOriginPath() + "trip/create",
+				data: myjson,
+				headers: {'Content-Type': 'application/x-www-form-urlencoded',
+				'Content-Type': 'application/json'}
+			}).
+			success(function(data, status, headers, config) {
+				createTripFactory.updateTrips();
+				createTripFactory.setIsEditingTrip(false);
+				createTripFactory.setChosenTrip({});
+				$scope.cancel();	
+			}).
+			error(function(data, status, headers, config) {
+				alert("Save Failed, Please Try Again.");
+				$scope.isDisabled = false;
+			});
+
+		}
+	}
+
+	$scope.publish = function (){
+		//TODO	
+	}
+});
+
+//=============================== Modal Controller ===============================
+
+var addTripAddPlaceModalInstanceCtrl = function ($scope, $modalInstance, createTripFactory) {
+
+	$scope.cancel = function () {
+		$modalInstance.dismiss('cancel');
+
+	};
+};
+
+var addTripEditPlaceModalInstanceCtrl = function ($scope, $modalInstance, createTripFactory) {
+
+	$scope.cancel = function () {
+		$modalInstance.dismiss('cancel');
+
+	};
+};
+
