e6f1dd08dbd690fa19d5327ee5b3378b242b8b91
JaegerJaqueZ <ny_t124@hotmail.com>
2014-04-15 17:17:15 +0700

    [Add] add modal edit place foursquare

---
+++ public/js/angular/controllers/create_trip/modal_edit_place_foursquare.js
@@ -0,0 +1,35 @@
+//public/js/angular/controllers/modal_edit_place_foursquare.js
+
+var modalEditPlaceFoursquareControllers = angular.module('modalEditPlaceFoursquareControllers', []);
+
+modalEditPlaceFoursquareControllers.controller('modalEditPlaceFoursquareCtrl', function ($scope, $http, createTripFactory) {
+
+	//initialze search (can delete if want)
+	$scope.input = {name:'paragon', province:'กรุงเทพมหานคร'};
+
+	$scope.findPlace = function (){
+
+		$http({method: 'GET', url: 'https://api.foursquare.com/v2/venues/explore?client_id=FSEL5ZQNNTPR4RHCVJMQ53541XJPZM4LIHBCNJVBVHRJTE4O&client_secret=KBIQX2U1X4LZ5GWRSAELWH2CFSTPBBNK4NBQZCGB2KQE1ENQ&v=20130619&query=' + $scope.input.name + '&near=' + $scope.input.province}).
+		success(function(data, status, headers, config) {
+
+			$scope.foursqplaces = data.response.groups[0].items;
+
+		}).
+		error(function(data, status, headers, config) {
+
+			alert("error");
+
+		});
+	}	
+	
+	$scope.selectPlace = function (chosenplace){	
+
+		var temp = createTripFactory.getChosenPlace();
+		temp.foursquare = jQuery.extend({}, createTripFactory.adjustPlaceObject(chosenplace).foursquare);	
+		
+		$scope.cancel();
+	}
+
+
+
+});
