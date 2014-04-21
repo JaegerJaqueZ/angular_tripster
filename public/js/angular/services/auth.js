02820e61147924a537b5549279e49facb95b540a
JaegerJaqueZ <ny_t124@hotmail.com>
2014-04-21 21:28:06 +0700

    [Add] auth service

---
+++ public/js/angular/services/auth.js
@@ -0,0 +1,86 @@
+//public/js/angular/services/login.js
+
+var authService = angular.module('authService', []);
+
+authService.factory('authFactory', function($location, $rootScope, $cookieStore, $http) {
+    
+//============================ Factory Attributes ==============================
+	
+	$rootScope.currentUser = $cookieStore.get('user') || null;
+    $cookieStore.remove('user');
+
+//============================== Factory Methods ===============================
+
+	function getOriginPath() {
+		return 'http://158.108.230.1:4000';
+	}
+
+	function getCurrentUser() {
+
+		console.log("getCurrentUser");
+		
+		$http({
+			method:'GET', 
+			url: getOriginPath() + "/api/session"
+		})
+		.success(function(data, status, headers, config) {
+			console.log(data);
+			$rootScope.currentUser = data;
+		})
+		.error(function(data, status, headers, config) {
+			console.log(data);
+			alert("Failed to login, Please Try Again");
+		}); 
+		
+	}
+
+	function login(provider, user, callback) {
+		var cb = callback || angular.noop;
+		// Session.save({
+		// 	provider: provider,
+		// 	email: user.email,
+		// 	password: user.password,
+		// 	rememberMe: user.rememberMe
+		// }, function(user) {
+		// 	$rootScope.currentUser = user;
+		// 	return cb();
+		// }, function(err) {
+		// 	return cb(err.data);
+		// });
+	}
+
+	function logout(callback) {
+		var cb = callback || angular.noop;
+		// Session.delete(function(res) {
+// 			$rootScope.currentUser = null;
+// 			return cb();
+// 		},
+// 		function(err) {
+// 			return cb(err.data);
+// 		});
+	}
+	
+	function signUp(userinfo, callback) {
+		var cb = callback || angular.noop;
+		// User.save(userinfo,
+// 			function(user) {
+// 				$rootScope.currentUser = user;
+// 				return cb();
+// 			},
+// 			function(err) {
+// 				return cb(err.data);
+// 			});
+	}
+	
+	
+
+//=============================== Factory Return ===============================
+
+    return {
+		getOriginPath: getOriginPath,
+		getCurrentUser: getCurrentUser,
+  		login: login,
+		logout: logout
+    }
+	
+  });
