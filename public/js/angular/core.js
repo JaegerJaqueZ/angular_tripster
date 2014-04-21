32554af69851295a3aa788807872190504ab5ad4
JaegerJaqueZ <ny_t124@hotmail.com>
2014-04-21 21:28:50 +0700

    [Modify][tripster.html and core.js] to support facebook login

--- public/js/angular/core.js
+++ public/js/angular/core.js
@@ -17,11 +17,14 @@ var tripsterApp = angular.module('tripsterApp', [
 	'modalEditPlaceFoursquareControllers',	
 	'searchTripControllers',
 	'modalSearchTripControllers',
-	'bookmarkControllers'
+	'bookmarkControllers',
+	'ngCookies',
+	'authService',
+	'loginControllers'
 ]);
 
-tripsterApp.config(['$routeProvider',
-function($routeProvider) {
+tripsterApp.config(['$routeProvider', '$locationProvider', '$httpProvider',
+function($routeProvider, $locationProvider, $httpProvider) {
 	$routeProvider.
 	when('/home', {
 		templateUrl: 'partials/home.html'
@@ -38,7 +41,46 @@ function($routeProvider) {
 		templateUrl: 'partials/bookmark.html',
 		controller: 'bookmarkCtrl'
 	}).
+	when('/login', {
+			templateUrl: 'partials/login.html',
+			controller: 'loginCtrl'
+	}).
 	otherwise({
 		redirectTo: '/home'
 	});
+
+	// $locationProvider.html5Mode(true);
+
+    $httpProvider.interceptors.push(function($q, $location) {
+        return {
+            'responseError': function(response) {
+                if(response.status === 401) {
+                    $location.path('/login');
+                }
+                return $q.reject(response);
+            }
+        };
+    });
+
 }]);
+
+
+tripsterApp.config(['$httpProvider', function($httpProvider) {
+        $httpProvider.defaults.useXDomain = true;
+		delete $httpProvider.defaults.headers.common['X-Requested-With'];
+    }
+]);
+
+
+tripsterApp.run(function ($rootScope, $location, authFactory) {
+
+    //watching the value of the currentUser variable.
+    $rootScope.$watch('currentUser', function(currentUser) {
+      // if no currentUser and on a page that requires authorization then try to update it
+      // will trigger 401s if user does not have a valid session
+      if (!currentUser && (['/', '/login', '/logout'].indexOf($location.path()) == -1 )) {
+      	authFactory.getCurrentUser();
+      }
+  });
+
+});
