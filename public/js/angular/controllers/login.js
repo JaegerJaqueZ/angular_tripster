9345ae3e732aa712802d67e23e06fefd923b355c
JaegerJaqueZ <ny_t124@hotmail.com>
2014-04-21 21:27:42 +0700

    [Add] login both html and js

---
+++ public/js/angular/controllers/login.js
@@ -0,0 +1,9 @@
+//public/js/angular/controllers/login.js
+
+var loginControllers = angular.module('loginControllers', []);
+
+loginControllers.controller('loginCtrl', function ($scope, $http, authFactory) {
+
+
+	
+});
