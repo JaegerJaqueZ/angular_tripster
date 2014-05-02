5bfb36082f76e5251b5fa3f23224487b994b5375
JaegerJaqueZ <ny_t124@hotmail.com>
2014-05-02 23:20:24 +0700

    [Modify] server to be run on angular

--- server.js
+++ server.js
@@ -1,17 +1,13 @@
 var express = require('express');
 
 // configuration ===============================================================
-var   app   = express() 								// create our app w/ express
-    , port  = process.env.PORT || 5000; 				// set the port
-
+var   app   = express(); 						  			// create our app w/ express
+ 
 app.configure(function() {
 	app.use(express.static(__dirname + '/public')); 		// set the static files location /public/img will be /img for users
 	app.use(express.logger('dev')); 						// log every request to the console
 	app.use(express.bodyParser()); 							// pull information from html in POST
 	app.use(express.methodOverride()); 						// simulate DELETE and PUT
-	app.use(function(req, res) {
-		res.sendfile(__dirname + '/public/html/tripster.html');
-	});
 });
 
 app.configure('development', function(){
@@ -21,7 +17,32 @@ app.configure('development', function(){
 // routes ======================================================================
 // require('./app/routes.js')(app);
 
-// listen (start app with node server.js) ======================================
-app.listen(port, function() {
-	console.log("App listening on port " + port);
+// application -------------------------------------------------------------
+app.get('/', function(req, res) {
+	res.sendfile('./public/html/tripster.html'); // load the single view file (angular will handle the page changes on the front-end)
 });
+
+app.get('/partials/:name', function (req, res) {
+  var name = req.params.name;
+  res.sendfile('./public/html/partials/' + name);
+});
+
+// listen (start app with node server.js) ======================================
+
+var os = require('os')
+
+var interfaces = os.networkInterfaces();
+var addresses = [];
+for (k in interfaces) {
+    for (k2 in interfaces[k]) {
+        var address = interfaces[k][k2];
+        if (address.family == 'IPv4' && !address.internal) {
+            addresses.push(address.address)
+        }
+    }
+}
+
+console.log(addresses)
+
+app.listen(8000);
+console.log("App listening on port 8000");
