15296ef10ffee6b923cf663f90faa46412d0d799
Panuwat Palipote <sthunder2@hotmail.com>
2014-05-09 22:26:13 +0700

    [Modify] add search nearby function

--- public/js/angular/controllers/create_trip/modal_edit_place_foursquare.js
+++ public/js/angular/controllers/create_trip/modal_edit_place_foursquare.js
@@ -6,6 +6,8 @@ modalEditPlaceFoursquareControllers.controller('modalEditPlaceFoursquareCtrl', f
 
 	//initialze search (can delete if want)
 	$scope.input = {province:"กรุงเทพมหานคร"};
+	var lat="",
+		lng="";		
 
 	$scope.findPlace = function (){
 
@@ -21,7 +23,30 @@ modalEditPlaceFoursquareControllers.controller('modalEditPlaceFoursquareCtrl', f
 
 		});
 	}	
-	
+
+	function getLocation(location) {
+	    lat = location.coords.latitude;
+	    lng = location.coords.longitude;
+				console.log(lat+','+lng);
+	}	
+
+	// search nearby place
+	$scope.findNearbyPlace = function (){
+		navigator.geolocation.getCurrentPosition(getLocation);
+		$http({method: 'GET', url: 'https://api.foursquare.com/v2/venues/explore?client_id=FSEL5ZQNNTPR4RHCVJMQ53541XJPZM4LIHBCNJVBVHRJTE4O&client_secret=KBIQX2U1X4LZ5GWRSAELWH2CFSTPBBNK4NBQZCGB2KQE1ENQ&v=20130619&ll=' +lat+','+lng+''}).
+		//$http({method: 'GET', url: 'https://api.foursquare.com/v2/venues/explore?client_id=FSEL5ZQNNTPR4RHCVJMQ53541XJPZM4LIHBCNJVBVHRJTE4O&client_secret=KBIQX2U1X4LZ5GWRSAELWH2CFSTPBBNK4NBQZCGB2KQE1ENQ&v=20130619&query=' + $scope.input.name + '&near=' + $scope.input.province}).
+		success(function(data, status, headers, config) {
+			console.log( data.response.groups[0].items);
+			$scope.foursqplaces = data.response.groups[0].items;
+
+		}).
+		error(function(data, status, headers, config) {
+
+			alert("error");
+
+		});
+	}		
+		
 	$scope.selectPlace = function (chosenplace){	
 
 		var temp = createTripFactory.getChosenPlace();
