97a0d5c5e11514d92e9ac506d199e8fd97a6608c
JaegerJaqueZ <ny_t124@hotmail.com>
2014-05-12 15:07:14 +0700

    [Modify] Add loading animation when search on 4sq

--- public/js/angular/controllers/create_trip/modal_add_place_foursquare.js
+++ public/js/angular/controllers/create_trip/modal_add_place_foursquare.js
@@ -8,17 +8,21 @@ modalAddPlaceFoursquareControllers.controller('modalAddPlaceFoursquareCtrl', fun
 	$scope.input = {province:"กรุงเทพมหานคร"};
 	var lat="",
 		lng="";	
+	//initialize loading spinner	
+	$scope.loading = false;
 
 	$scope.findPlace = function (){
 
+		$scope.loading = true;
+
 		$http({method: 'GET', url: 'https://api.foursquare.com/v2/venues/explore?client_id=FSEL5ZQNNTPR4RHCVJMQ53541XJPZM4LIHBCNJVBVHRJTE4O&client_secret=KBIQX2U1X4LZ5GWRSAELWH2CFSTPBBNK4NBQZCGB2KQE1ENQ&v=20130619&query=' + $scope.input.name + '&near=' + $scope.input.province}).
 		success(function(data, status, headers, config) {
-
+			$scope.loading = false;
 			$scope.foursqplaces = data.response.groups[0].items;
-
+			
 		}).
 		error(function(data, status, headers, config) {
-
+			$scope.loading = false;
 			alert("error");
 
 		});
@@ -26,6 +30,7 @@ modalAddPlaceFoursquareControllers.controller('modalAddPlaceFoursquareCtrl', fun
 
 	// search nearby place
 	$scope.findNearbyPlace = function (){
+		$scope.loading = true;
 		navigator.geolocation.getCurrentPosition(geoSuccess, geoFail);
 	}		
 
@@ -36,11 +41,13 @@ modalAddPlaceFoursquareControllers.controller('modalAddPlaceFoursquareCtrl', fun
 	    $http({method: 'GET', url: 'https://api.foursquare.com/v2/venues/explore?client_id=FSEL5ZQNNTPR4RHCVJMQ53541XJPZM4LIHBCNJVBVHRJTE4O&client_secret=KBIQX2U1X4LZ5GWRSAELWH2CFSTPBBNK4NBQZCGB2KQE1ENQ&v=20130619&ll=' +lat+','+lng+''}).
 		//$http({method: 'GET', url: 'https://api.foursquare.com/v2/venues/explore?client_id=FSEL5ZQNNTPR4RHCVJMQ53541XJPZM4LIHBCNJVBVHRJTE4O&client_secret=KBIQX2U1X4LZ5GWRSAELWH2CFSTPBBNK4NBQZCGB2KQE1ENQ&v=20130619&query=' + $scope.input.name + '&near=' + $scope.input.province}).
 		success(function(data, status, headers, config) {
-			// console.log( data.response.groups);
+			$scope.loading = false;
+			console.log( data.response.groups);
 			$scope.foursqplaces = data.response.groups[0].items;
-
+			
 		}).
 		error(function(data, status, headers, config) {
+			$scope.loading = false;
 			// console.log(data);
 			alert('Something went wrong. Please try again.');
 		});
@@ -48,6 +55,7 @@ modalAddPlaceFoursquareControllers.controller('modalAddPlaceFoursquareCtrl', fun
 	}
 
 	function geoFail() {
+		$scope.loading = false;
 		alert('Cannot get your current location. Please try again.');
 	}
 
